d005f86c042eadc55317f47db5bf96fb189a0028
Olzhas <oljasalidarov@gmail.com>
2022-03-25 21:57:55 +0600

    day 1

--- src/components/PhonesComp.vue
+++ src/components/PhonesComp.vue
@@ -1,18 +1,29 @@
 <template>
+  <div class="bg-secondBG py-10">
     <div class="container mx-auto">
-        <div v-for="phone of allPhones" :key="phone.id">
-            <h3>{{ phone.name }}</h3>
+      <h2 class="text-2xl font-black mb-5">Смартфоны</h2>
+      <div class="flex flex-wrap items-start justify-between">
+        <div
+          class="bg-white rounded-lg p-2 phone mx-1 my-2"
+          v-for="phone of allPhones"
+          :key="phone.id"
+        >
+          <img :src="phone.images[0].first" alt="" />
+          <h3 class="font-semibold">{{ phone.name }}</h3>
+          <p class="text-2xl font-bold">{{ phone.price }} тенге</p>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from "vuex";
 export default {
-    computed: mapGetters(['allPhones']),
-    async mounted() {
-        this.fetchPhones();
-    },
-    methods: mapActions(['fetchPhones'])
-}
+  computed: mapGetters(["allPhones"]),
+  async mounted() {
+    this.fetchPhones();
+  },
+  methods: mapActions(["fetchPhones"]),
+};
 </script>
--- src/store/catalog.js
+++ src/store/catalog.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 export default {
-    state: {
-        phones: []
-    },
-    getters: {
-        allPhones(state) {
-            return state.phones
+    actions: {
+        async fetchPhones(result) {
+            const res = await axios.get(`http://localhost:3001/catalog`);
+            const newPhones = res.data;
+            result.commit('updatePhones', newPhones);
         }
     },
     mutations: {
@@ -13,11 +12,12 @@ export default {
             state.phones = newPhones
         }
     },
-    actions: {
-        async fetchPhones(result) {
-            const res = await axios.get(`http://localhost:3001/catalog`);
-            const newPhones = res.data;
-            result.commit('updatePhones', newPhones);
+    state: {
+        phones: []
+    },
+    getters: {
+        allPhones(state) {
+            return state.phones
         }
     },
 }
