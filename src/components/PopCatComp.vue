b4233540856040d5576fb3a25c19838ca99b0b64
Olzhas <oljasalidarov@gmail.com>
2022-03-28 21:53:38 +0600

    final day 2

---
+++ src/components/PopCatComp.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h2 class="text-2xl font-black mb-2">Популярные категории</h2>
+      <p class="mb-4">Широкий спектр товаров на любой вкус</p>
+      <div class="flex flex-wrap items-start justify-between">
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/f0767742-79d2-11ec-abd6-02420a01346a" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/db7d5df8-79d7-11ec-abd6-02420a01346a" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/7d1b3b6d-a50c-11ec-94d1-1abc33bc92e2" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/9c14bd61-a50d-11ec-94d1-1abc33bc92e2" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/d51b0bf4-a5cd-11ec-be7f-b2eadcb7f13d" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/c9ff71de-a50c-11ec-94d1-1abc33bc92e2" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/6449c92c-a50e-11ec-be7f-b2eadcb7f13d" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/126e7520-79de-11ec-abd6-02420a01346a" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/1bce88d3-7a71-11ec-abd6-02420a01346a" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/939b8dc2-a50c-11ec-94d1-1abc33bc92e2" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/d9efcde8-a4e7-11ec-be7f-b2eadcb7f13d" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/68a466e0-7a7e-11ec-abd6-02420a01346a" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/8ab97cbc-a50d-11ec-94d1-1abc33bc92e2" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/733f0d63-a50d-11ec-be7f-b2eadcb7f13d" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/8e25572f-7a9c-11ec-abd6-02420a01346a" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/50604492-a50e-11ec-94d1-1abc33bc92e2" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/a57d4c8e-a50c-11ec-be7f-b2eadcb7f13d" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="https://api.technodom.kz/f3/api/v1/images/c832b0b1-a50d-11ec-94d1-1abc33bc92e2" alt="">
+          </div>
+          <!-- <div class="singleBlock  p-2 bg-whitemb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div>
+          <div class="singleBlock p-2 bg-white mb-2 hover:border hover:border-mainCol rounded-md">
+              <img class="w-full" src="" alt="">
+          </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopCatComp",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <sliders />
     <phones />
+    <popular />
   </div>
 </template>
 
 <script>
 import phones from "../components/PhonesComp.vue";
-import sliders from "../components/SlidersComp.vue"
+import sliders from "../components/SlidersComp.vue";
+import popular from "../components/PopCatComp.vue"
 export default {
   name: "HomeView",
   components: {
     phones,
-    sliders
+    sliders,
+    popular
   },
 };
 </script>
