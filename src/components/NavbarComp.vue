f0aa431a00b32422583d35432aacb74444c7d0a8
Olzhas <oljasalidarov@gmail.com>
2022-04-11 20:03:25 +0600

    register

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -40,16 +40,23 @@
             <i class="fas fa-search search-but"></i>
           </div>
           <div class="flex">
-            <div class="text-center mx-3 text-grayText hover:text-mainCol hover:cursor-pointer">
+            <div
+              class="text-center mx-3 text-grayText hover:text-mainCol hover:cursor-pointer"
+            >
               <i class="fas fa-heart"></i>
               <p>Избранное</p>
             </div>
-            <div class="text-center mx-3 text-grayText hover:text-mainCol hover:cursor-pointer">
+            <div
+              class="text-center mx-3 text-grayText hover:text-mainCol hover:cursor-pointer"
+            >
               <i class="fas fa-balance-scale-left"></i>
               <p>Сравнить</p>
             </div>
             <div class="text-center mx-3 relative">
-              <div class="hover:text-mainCol hover:cursor-pointer text-grayText" @click="shoFunc()">
+              <div
+                class="hover:text-mainCol hover:cursor-pointer text-grayText"
+                @click="shoFunc()"
+              >
                 <p class="counter">{{ showCounter }}</p>
                 <i class="fas fa-shopping-cart"></i>
                 <p>Корзина</p>
@@ -71,25 +78,49 @@
                     />
                     <p class="mx-3 text-xs">{{ products.name }}</p>
                     <p class="font-bold">{{ products.price }}т.</p>
-                    <p @click="removeItem(index)" class="ml-3 bg-red-600 px-2 p-1 rounded-md"><i class="fa-solid fa-xmark text-lg text-white"></i></p>
+                    <p
+                      @click="removeItem(index)"
+                      class="ml-3 bg-red-600 px-2 p-1 rounded-md"
+                    >
+                      <i class="fa-solid fa-xmark text-lg text-white"></i>
+                    </p>
                   </div>
                 </div>
                 <p class="text-right">Итого: {{ sumOfProducts }}</p>
                 <div class="flex justify-between items-center">
-                  <router-link class="hover:cursor-pointer hover:text-blue-400" to="/cart">Открыть корзину</router-link>
-                  <p @click="removeAll()" class="hover:cursor-pointer hover:text-blue-400">Очистить корзину</p>
+                  <router-link
+                    class="hover:cursor-pointer hover:text-blue-400"
+                    to="/cart"
+                    >Открыть корзину</router-link
+                  >
+                  <p
+                    @click="removeAll()"
+                    class="hover:cursor-pointer hover:text-blue-400"
+                  >
+                    Очистить корзину
+                  </p>
                 </div>
               </div>
             </div>
-            <div class="text-center mx-3 text-grayText hover:text-mainCol hover:cursor-pointer">
-              <div @click="currentUser = null" v-if="currentUser != null" class="block">
+            <div
+              class="text-center mx-3 text-grayText hover:text-mainCol hover:cursor-pointer"
+            >
+              <div
+                @click="currentUser = null"
+                v-if="currentUser != null"
+                class="block"
+              >
                 <div class="flex items-center justify-between">
                   <i class="fas fa-user"></i>
                   <p>{{ currentUser.name }}</p>
                 </div>
                 <p>Бонусы: {{ currentUser.bonuses }}</p>
               </div>
-              <div data-bs-toggle="modal" data-bs-target="#exampleModal" v-if="currentUser == null">
+              <div
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                v-if="currentUser == null"
+              >
                 <i class="fas fa-user"></i>
                 <p>Вход</p>
               </div>
@@ -99,41 +130,118 @@
       </div>
     </div>
     <div>
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header flex justify-between items-center">
-              <button @click="showForm = 1" :class="{ 'border-mainCol' : showForm === 1 }" class="border rounded-lg py-3 w-full mr-2">Вход</button>
-              <button @click="showForm = 2" :class="{ 'border-mainCol' : showForm === 2 }" class="border rounded-lg py-3 w-full ml-2">Регистрация</button>
+              <button
+                @click="showForm = 1"
+                :class="{ 'border-mainCol': showForm === 1 }"
+                class="border rounded-lg py-3 w-full mr-2"
+              >
+                Вход
+              </button>
+              <button
+                @click="showForm = 2"
+                :class="{ 'border-mainCol': showForm === 2 }"
+                class="border rounded-lg py-3 w-full ml-2"
+              >
+                Регистрация
+              </button>
             </div>
             <div class="modal-body">
-              <div v-if="valid_form.password === 1 || valid_form.email === 1" class="bg-red-400 text-white w-full p-2 rounded-lg">
+              <div
+                v-if="valid_form.password === 1 || valid_form.email === 1"
+                class="bg-red-400 text-white w-full p-2 rounded-lg"
+              >
                 <h2 class="text-lg">Вы ввели неправильный email или пароль</h2>
                 <p>Попробуйте снова</p>
               </div>
               <form v-if="showForm === 1" action="">
-                <input v-model="form.email" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="text" placeholder="E-mail">
-                <input v-model="form.password" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="password" placeholder="Пароль">
-                <p @click="loginForm()" class="bg-mainCol p-2 w-full rounded-md text-center text-white uppercase">Войти</p>
+                <input
+                  v-model="form.email"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="text"
+                  placeholder="E-mail"
+                />
+                <input
+                  v-model="form.password"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="password"
+                  placeholder="Пароль"
+                />
+                <p
+                  @click="loginForm()"
+                  class="bg-mainCol p-2 w-full rounded-md text-center text-white uppercase"
+                >
+                  Войти
+                </p>
               </form>
               <form v-if="showForm === 2" action="">
-              <div class="bg-red-500 text-white p-3 rounded-lg text-sm">
-                Неверная регистрация
-              </div>
-              <div class="bg-green-500 text-white p-3 rounded-lg text-sm">
-                Вы успешно зарегистрировались
-              </div>
-                <input v-model="register.name" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="text" placeholder="Имя">
-                <input v-model="register.surname" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="text" placeholder="Фамилия">
-                <input v-model="register.email" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="email" placeholder="E-mail">
-                <input v-model="register.phone" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="number" placeholder="Номер телефона">
-                <input v-model="register.pass" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="password" placeholder="Пароль">
-                <input v-model="register.confirmPass" class="block border border-gray-200 p-2 my-4 w-full rounded-md" type="password" placeholder="Подтвердите пароль">
-                <p @click="addUser()" class="bg-mainCol p-2 w-full rounded-md text-center text-white uppercase">Регистрация</p>
+                <div v-if="isActive === 2" class="bg-red-500 text-white p-3 rounded-lg text-sm">
+                  Неверная регистрация
+                </div>
+                <div v-if="isActive === 1" class="bg-green-500 text-white p-3 rounded-lg text-sm">
+                  Вы успешно зарегистрировались
+                </div>
+                <input
+                  v-model="register.name"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="text"
+                  placeholder="Имя"
+                />
+                <input
+                  v-model="register.surname"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="text"
+                  placeholder="Фамилия"
+                />
+                <input
+                  v-model="register.email"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="email"
+                  placeholder="E-mail"
+                />
+                <input
+                  v-model="register.phone"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="number"
+                  placeholder="Номер телефона"
+                />
+                <input
+                  v-model="register.pass"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="password"
+                  placeholder="Пароль"
+                />
+                <input
+                  v-model="register.confirmPass"
+                  class="block border border-gray-200 p-2 my-4 w-full rounded-md"
+                  type="password"
+                  placeholder="Подтвердите пароль"
+                />
+                <p
+                  @click="addUser()"
+                  class="bg-mainCol p-2 w-full rounded-md text-center text-white uppercase"
+                >
+                  Регистрация
+                </p>
               </form>
             </div>
             <div class="modal-footer">
-              <button class="w-full text-center text-sm" type="button" data-bs-dismiss="modal">Закрыть</button>
+              <button
+                class="w-full text-center text-sm"
+                type="button"
+                data-bs-dismiss="modal"
+              >
+                Закрыть
+              </button>
             </div>
           </div>
         </div>
@@ -144,7 +252,7 @@
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "NavbarComp",
@@ -156,9 +264,10 @@ export default {
       showForm: 1,
       users: [],
       currentUser: null,
+      isActive: 0,
       form: {
         email: "",
-        password: ""
+        password: "",
       },
       register: {
         name: "",
@@ -167,21 +276,21 @@ export default {
         phone: "",
         pass: "",
         confirmPass: "",
-        bonuses: 0
+        bonuses: 0,
       },
       valid_form: {
         email: 0,
-        password: 0
-      }
+        password: 0,
+      },
     };
   },
   async mounted() {
-      const res = await axios.get(`http://localhost:3001/users`);
-      this.users = res.data;
-      console.log(this.users);
+    const res = await axios.get(`http://localhost:3001/users`);
+    this.users = res.data;
+    console.log(this.users);
   },
   methods: {
-    ...mapActions(['removeByIndex', 'removeAllCart']),
+    ...mapActions(["removeByIndex", "removeAllCart"]),
     shoFunc() {
       if (this.showCart === 0) {
         this.showCart = 1;
@@ -191,44 +300,47 @@ export default {
     },
     removeItem(id) {
       this.removeByIndex(id);
-      console.log(id)
+      console.log(id);
     },
     removeAll() {
       this.removeAllCart();
     },
     loginForm() {
-      for(let i = 0; i <= this.users.length; i++) {
-        if(this.users[i].email == this.form.email && this.users[i].pass == this.form.password) {
+      for (let i = 0; i <= this.users.length; i++) {
+        if (
+          this.users[i].email == this.form.email &&
+          this.users[i].pass == this.form.password
+        ) {
           this.currentUser = this.users[i];
           this.valid_form.email = 0;
           this.valid_form.password = 0;
-          console.log("yes")
-        } else if(this.currentUser != null) {
+          console.log("yes");
+        } else if (this.currentUser != null) {
           this.valid_form.email = 0;
           this.valid_form.password = 0;
         } else {
           this.valid_form.email = 1;
           this.valid_form.password = 1;
-          console.log("no")
+          console.log("no");
         }
       }
     },
     async addUser() {
-      try {
-        const res = await axios.post('http://localhost:3001/users', 
-        { 
+      if (this.register.pass === this.register.confirmPass) {
+        this.isActive = 1;
+        const res = await axios.post("http://localhost:3001/users", {
           name: this.register.name,
           surname: this.register.surname,
           email: this.register.email,
           phone: this.register.phone,
           pass: this.register.pass,
-          bonuses: this.register.bonuses
+          bonuses: this.register.bonuses,
         });
         this.register = [...this.register, res.data];
-      } catch (e) {
-        console.error(e);
+      } else {
+        this.isActive = 2;
       }
-    }
+    },
   },
 };
 </script>
